ffb406b4cd64b95fb4c3c24b2b2d181defd7bcdf
prvdnx <129741476+Prvdnx@users.noreply.github.com>
2025-07-03 00:42:33 +0700

    Add reset button functionality and style adjustments to Basketball Scoreboard

--- Javascript/08-Solo Projects/Solo Project - Basketball Scoreboard/index.js
+++ Javascript/08-Solo Projects/Solo Project - Basketball Scoreboard/index.js
@@ -1,14 +1,8 @@
 
-// const plus1Home = document.getElementById("plus1-home")
-// const plus2Home = document.getElementById("plus2-home")
-// const plus3Home = document.getElementById("plus3-home")
 const homeBtn = document.getElementsByClassName("home-btn")
 const homeScore = document.getElementById("home-score")
 let hScore = 0
 
-// const plus1Guest = document.getElementById("plus1-guest")
-// const plus2Guest = document.getElementById("plus2-guest")
-// const plus3Guest = document.getElementById("plus3-guest")
 const guestBtn = document.getElementsByClassName("guest-btn")
 const guestScore = document.getElementById("guest-score")
 let gScore = 0
@@ -50,5 +44,4 @@ for (let btn of homeBtn) {
     console.log(btn)
 }
 
-// const buttonText = plusOneButton.innerText;
 // let btnScore = +btn.innerText;
