c6bbecca0b8fcdc478b64e91628e17876815e9d5
prvdnx <129741476+Prvdnx@users.noreply.github.com>
2025-07-04 19:56:55 +0700

    Implement password generation and clipboard copy functionality

--- Javascript/08-Solo Projects/Solo Project - Password Generator/index.js
+++ Javascript/08-Solo Projects/Solo Project - Password Generator/index.js
@@ -2,5 +2,43 @@ const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O"
 "/"];
 
 
+const genBtn = document.getElementById("generate-btn")
+const passWords = document.getElementsByClassName("passwords")
+
+genBtn.addEventListener("click", function() {
+    for (let pass of passWords) {
+        genPass(pass)
+    }
+})
+
+for (let pass of passWords) {
+    pass.addEventListener("click", function() {
+        if (pass.innerText.length > 1) {
+            navigator.clipboard.writeText(pass.innerText)
+            console.log('Content copied to clipboard successfully!');
+            alert('Copied successfully!');
+        } else {
+            console.log('Copy unsuccessfull OR Nothing to copy!');
+        }
+    })
+}
+
+function genPass(pass) {
+    let myPass = ""
+    for (let i = 0; i < 15; i++) {
+        myPass += characters[Math.floor(Math.random() * characters.length)]
+    }
+    pass.innerText = myPass
+}
 
 
+// copyButton.addEventListener('click', () => {
+//     navigator.clipboard.writeText(textToCopy.value)
+////     navigator.clipboard.writeText(textToCopy.innerText)
+//         .then(() => {
+//             console.log('Copied successfully!');
+//         })
+//         .catch(err => {
+//             console.error('Failed to copy content: ', err);
+//         });
+// });
