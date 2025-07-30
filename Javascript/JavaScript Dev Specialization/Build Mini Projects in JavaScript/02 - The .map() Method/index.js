2ed31b7f4ee4811d71d52680d22a6148413f6c34
prvdnx <129741476+Prvdnx@users.noreply.github.com>
2025-07-31 03:32:47 +0700

    mini js projects

---
+++ Javascript/JavaScript Dev Specialization/Build Mini Projects in JavaScript/02 - The .map() Method/index.js
@@ -0,0 +1,19 @@
+//Convert these Miles to KM! 
+const distanceWalkedMilesArr = [140, 153, 161, 153, 128, 148]
+
+const conversionFactorMilesToKm = 1.6
+
+
+// const distanceWalkedKmArr = distanceWalkedMilesArr.map(function(distanceMiles, index){
+//     return `Month ${index}: ${distanceMiles * conversionFactorMilesToKm}KM`
+// })
+
+// console.log(distanceWalkedKmArr)
+
+function convertMilesToKms() {
+    return distanceWalkedMilesArr.map(function (distanceMiles, index) {
+        return `Month ${index}: ${distanceMiles * conversionFactorMilesToKm}KM`
+    })
+}
+
+console.log(convertMilesToKms())
