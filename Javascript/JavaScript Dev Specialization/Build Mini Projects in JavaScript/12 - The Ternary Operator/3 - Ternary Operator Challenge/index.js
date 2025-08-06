0ec33caef4eef99e819489175ab2717e2731d1f7
prvdnx <129741476+Prvdnx@users.noreply.github.com>
2025-08-07 03:28:27 +0700

    done with tenary op

---
+++ Javascript/JavaScript Dev Specialization/Build Mini Projects in JavaScript/12 - The Ternary Operator/3 - Ternary Operator Challenge/index.js
@@ -0,0 +1,30 @@
+const playerGuess = 9
+const correctAnswer = 6
+
+/*
+Challenge 
+1. Refactor the if else statement to use a ternary operator.
+*/
+// let message = ''
+// if (playerGuess === correctAnswer) {
+//     message = 'Correct!'
+// }
+// else {
+//     message = 'Wrong!'
+// }
+
+// const message = playerGuess === correctAnswer ? 'Correct!' : 'Wrong!'
+
+
+/*
+Challenge
+1. Now improve the functionality of this code by 
+   letting the player know if their guess was too high, 
+   too low, or exactly right.
+*/
+
+const message = playerGuess < correctAnswer ? 'Too low!' 
+    : playerGuess > correctAnswer ? 'Too high!' 
+    : 'Exactly right!'
+
+console.log(message)
