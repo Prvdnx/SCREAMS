f00e047a92be56af7e1df62829f70649ab6dfab6
prvdnx <129741476+Prvdnx@users.noreply.github.com>
2025-08-07 04:32:18 +0700

    Short-circuiting with OR (||)

---
+++ Javascript/JavaScript Dev Specialization/Build Mini Projects in JavaScript/15 - Short-circuiting with OR (||) and AND (&&)/1 - Short-circuiting with OR (||)/index.js
@@ -0,0 +1,21 @@
+const jobHunter = {
+    name: 'Tom Chant',
+    jobSearchArea: 'Europe',
+}
+
+/***** if else *****/
+// if (jobHunter.jobSearchArea) {
+//     console.log(`${jobHunter.name}'s work location is ${jobHunter.jobSearchArea}`)
+// }
+// else {
+//     console.log(`${jobHunter.name}'s work location is Worldwide`)
+// }
+
+/***** ternary *****/
+// const workLocation = jobHunter.jobSearchArea ? jobHunter.jobSearchArea : 'Worldwide' 
+// console.log(`${jobHunter.name}'s work location is ${workLocation}`)
+
+
+/***** Short-circuiting with OR (||) *****/
+const workLocation = jobHunter.jobSearchArea || 'Worldwide'
+console.log(`${jobHunter.name}'s work location is ${workLocation}`)
