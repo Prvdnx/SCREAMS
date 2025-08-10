2a922fff0b97209e6cf70cc29e314ebff8c5e06b
prvdnx <129741476+Prvdnx@users.noreply.github.com>
2025-08-11 01:11:55 +0700

    Why React? It's Declarative!

---
+++ React/Learn React/module 1/6 - Why React? It's Declarative!/index.jsx
@@ -0,0 +1,35 @@
+        // React is Declarative //
+
+import { createRoot } from "react-dom/client"
+const root = createRoot(document.getElementById("root"))
+
+root.render(
+    <h1 className="header">Hello, React!</h1>
+)
+
+
+        // This is Imperative //
+/* 
+Challenge - recreate the above line of code in vanilla JS by creating and
+appending an h1 to our div#root (without using innerHTML).
+
+- Create a new h1 element (createElement)
+- Give it some textContent
+- Give it a class name of "header"
+- append it as a child (using `appendChild`) of the div#root
+
+Don't use innerHTML to accomplish any of this.
+    
+*/
+
+// const h1 = document.createElement("h1");
+// h1.textContent = "I'm Learning React!";
+// h1.className = "header"
+// const myRoot = document.getElementById("root");
+// myRoot.appendChild(h1);
+
+
+// const h1 = document.createElement("h1")
+// h1.textContent = "This is imperative coding"
+// h1.className = "header"
+// document.getElementById("root").appendChild(h1)
