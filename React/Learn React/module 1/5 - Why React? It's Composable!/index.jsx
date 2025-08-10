ccf5b432914cc5f7ffc844752c3b96a1ee7c93a4
prvdnx <129741476+Prvdnx@users.noreply.github.com>
2025-08-11 00:55:05 +0700

    .

--- React/Learn React/module 1/5 - Why React? It's Composable!/index.jsx
+++ React/Learn React/module 1/5 - Why React? It's Composable!/index.jsx
@@ -39,6 +39,14 @@ function MyAwesomeNavbar() {
     )
 }
 
+/**
+ * Challenge:
+ * Create your very first custom React component!
+ * Call it "MainContent", and have it return a simple
+ * h1 element that says "React is great!"
+ * 
+ * Afterward, render it on the line below MyAwesomeNavbar
+ */
 
 function MainContent() {
     return <h1>React is great!</h1>
