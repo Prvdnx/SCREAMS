d37116c2f4173c3dc4329770bbc7e2d64ec8a293
prvdnx <129741476+Prvdnx@users.noreply.github.com>
2025-10-01 11:03:11 +0700

    Styling with Classes

---
+++ React/Learn React/module 2/08-Styling with Classes/index.jsx
@@ -0,0 +1,66 @@
+import { createRoot } from "react-dom/client"
+const root = createRoot(document.getElementById("root"))
+
+/**
+ * Challenge:
+ * 
+ * - Add a `nav` > `ul` > `li` (x3). The 3 items should say:
+ *   "Pricing", "About", and "Contact"
+ * 
+ * Using flexbox, line up the `li`s horizontally and put them inline
+ * with the React logo.
+ * NOTE: for practice's sake, don't select any
+ * elements, but use classes for all styling.
+ * 
+ * Move the `width` style off the JSX and into CSS with a dedicated
+ * className on the img element, and change the width to 55px instead
+ */
+
+function Header() {
+    return (
+        <header className="header">
+            <img src="react-logo.png" className="nav-logo" alt="React logo" />
+            <nav>
+                <ul className="nav-list">
+                    <li className="nav-list-item">Pricing</li>
+                    <li className="nav-list-item">About</li>
+                    <li className="nav-list-item">Contact</li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
+
+function MainContent() {
+    return (
+        <main>
+            <h1>Reason I am excited to learn React</h1>
+            <ol>
+                <li>React is a popular library, so I will be able to fit in with all the coolest devs out there! 😎</li>
+                <li>I am more likely to get a job as a front end developer if I know React</li>
+            </ol>
+        </main>
+    )
+}
+
+function Footer() {
+    return (
+        <footer>
+            <small>© 2024 Ziroll development. All rights reserved.</small>
+        </footer>
+    )
+}
+
+function Page() {
+    return (
+        <>
+            <Header />
+            <MainContent />
+            <Footer />
+        </>
+    )
+}
+
+root.render(
+    <Page />
+)
