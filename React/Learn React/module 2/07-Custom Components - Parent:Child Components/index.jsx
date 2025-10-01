4ab1882c3fef4d68b1f278043c171a8f9be5cf62
prvdnx <129741476+Prvdnx@users.noreply.github.com>
2025-10-01 10:23:00 +0700

    Custom Components - Parent/Child Components

---
+++ React/Learn React/module 2/07-Custom Components - Parent:Child Components/index.jsx
@@ -0,0 +1,79 @@
+import { createRoot } from "react-dom/client"
+const root = createRoot(document.getElementById("root"))
+
+/** Challenge: 
+ * Move the `header` element from the Page component into 
+ * its own component called "Header"
+ * 
+ * Move the `main` element into its own component called "MainContent" 
+ * and render that component inside the Page component.
+ * 
+ * Do the same with the `footer` element, moving it into a new
+ * component called "Footer"
+ * 
+ * Then render an instance of all three components inside
+ * the Page component
+*/
+
+// function Page() {
+//     return (
+//         <>
+//             <header>
+//                 <img src="react-logo.png" width="40px" alt="React logo" />
+//             </header>
+//             <main>
+//                 <h1>Reason I am excited to learn React</h1>
+//                 <ol>
+//                     <li>React is a popular library, so I will be able to fit in with all the coolest devs out there! 😎</li>
+//                     <li>I am more likely to get a job as a front end developer if I know React</li>
+//                 </ol>
+//             </main>
+//             <footer>
+//                 <small>© 2024 Ziroll development. All rights reserved.</small>
+//             </footer>
+//         </>
+//     )
+// }
+
+
+function Header() {
+    return (
+        <header>
+            <img src="react-logo.png" width="40px" alt="React logo" />
+        </header>
+    )
+}
+
+function MainContent() {
+    return (
+        <main>
+            <h1>Reason I am excited to learn React</h1>
+            <ol>
+                <li>React is a popular library, so I will be able to fit in with all the coolest devs out there! 😎</li>
+                <li>I am more likely to get a job as a front end developer if I know React</li>
+            </ol>
+        </main>
+    )
+}
+
+function Footer() {
+    return (
+        <footer>
+            <small>© 2024 Ziroll development. All rights reserved.</small>
+        </footer>
+    )
+}
+
+function Page() {
+    return (
+        <>
+            <Header />
+            <MainContent />
+            <Footer />
+        </>
+    )
+}
+
+root.render(
+    <Page />
+)
