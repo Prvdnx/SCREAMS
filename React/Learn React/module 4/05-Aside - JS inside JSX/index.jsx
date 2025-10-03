5b04bc2ab84cbed3d2ad55a7f9bfc2a1ec6b9bf6
prvdnx <129741476+Prvdnx@users.noreply.github.com>
2025-10-03 14:28:11 +0700

    Aside - JS inside JSX

---
+++ React/Learn React/module 4/05-Aside - JS inside JSX/index.jsx
@@ -0,0 +1,42 @@
+import ReactDOM from 'react-dom/client';
+
+//=======================
+// function App() {
+//   const firstName = "Joe"
+//   const lastName = "Schmoe"
+  
+//   return (
+//     <h1>Hello {firstName} {lastName}</h1>
+//   )
+// }
+
+//=======================
+// function App() {
+//   const hours = new Date().getHours() % 12
+  
+//   return (
+//     <h1>It is currently about {hours}</h1>
+//   )
+// }
+
+//=======================
+function App() {
+  const hours = new Date().getHours()
+  let timeOfDay
+
+  if (hours < 12) {
+    timeOfDay = "morning"
+  } else if (hours >= 12 && hours < 17) {
+    timeOfDay = "afternoon"
+  } else if (hours < 21) {
+    timeOfDay = "evening"
+  } else {
+    timeOfDay = "night"
+  }
+
+  return (
+    <h1>Good {timeOfDay}</h1>
+  )
+}
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);
