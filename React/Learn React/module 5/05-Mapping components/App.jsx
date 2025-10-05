7e4ea438cd6572225bc5c08e5c7c1dbbf72f8567
prvdnx <129741476+Prvdnx@users.noreply.github.com>
2025-10-05 16:12:53 +0700

    Mapping components

---
+++ React/Learn React/module 5/05-Mapping components/App.jsx
@@ -0,0 +1,20 @@
+import Joke from "./Joke"
+import jokesData from "./jokesData"
+
+export default function App() {
+
+    /**
+     * Challenge: See if you can correctly pass the necessary props to the 
+     * Joke component in the .map() (and render the jokeElements array) so 
+     * the jokes show up on the page again
+     */    
+
+    const jokeElements = jokesData.map((joke) => {
+        return <Joke setup={joke.setup} punchline={joke.punchline} />
+    })
+    return (
+        <main>
+            {jokeElements}
+        </main>
+    )
+}
