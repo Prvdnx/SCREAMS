3473b59a01c43fbb1b88f801055d4e2932da5224
prvdnx <129741476+Prvdnx@users.noreply.github.com>
2025-10-12 16:54:42 +0700

    Conditional rendering practice - &&

---
+++ React/Learn React/07 - React State 3/04-Conditional rendering practice - &&/App.jsx
@@ -0,0 +1,30 @@
+import React from "react"
+
+export default function App() {
+    const [unreadMessages, setUnreadMessages] = React.useState([])
+
+    /**
+     * Challenge:
+     * If there are 0 unread messages, display a paragraph that says "You
+     * have no unread messages". (So, the logic will be the opposite of
+     * what we have for the h1)
+     */
+
+    /**
+     * Challenge:
+     * Only display the <h1> below if there are unread messages
+     */
+
+    return (
+        <div>
+            {
+                unreadMessages.length > 0 && 
+                <h1>You have {unreadMessages.length} unread messages!</h1>
+            }
+            {
+                unreadMessages.length === 0 && 
+                <p>You have no unread messages</p>
+            }
+        </div>
+    )
+}
