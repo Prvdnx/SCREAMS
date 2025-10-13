a1eddedb04f44d82b66b055ac1b89f24411e1a3a
prvdnx <129741476+Prvdnx@users.noreply.github.com>
2025-10-14 00:53:06 +0700

    useEffect quiz!

---
+++ React/Learn React/10 - React Side Effects 2/05 - useEffect quiz!/App.jsx
@@ -0,0 +1,25 @@
+import React from "react"
+
+export default function App() {
+    const [starWarsData, setStarWarsData] = React.useState({})
+    const [count, setCount] = React.useState(0)
+    
+    // add an item to a list in the database
+    
+    console.log("Rendered!")
+    
+    React.useEffect(() => {
+        console.log("Effect ran")
+        fetch("https://swapi.dev/api/people/1")
+            .then(res => res.json())
+            .then(data => setStarWarsData(data))
+    }, []) // [] compared to []
+    
+    return (
+        <div>
+            <h2>The count is {count}</h2>
+            <button onClick={() => setCount(prevCount => prevCount + 1)}>Add</button>
+            <pre>{JSON.stringify(starWarsData, null, 2)}</pre>
+        </div>
+    )
+}
