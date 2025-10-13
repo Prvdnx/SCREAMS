c5bcef020339499f85d645379914b27947d34fce
prvdnx <129741476+Prvdnx@users.noreply.github.com>
2025-10-13 14:04:11 +0700

    Format recipe response (from AI)

---
+++ React/Learn React/08 - React State 4/12-Format recipe response/components/ClaudeRecipe.jsx
@@ -0,0 +1,22 @@
+import ReactMarkdown from "react-markdown"
+
+/**
+ * Challenge: See if you can figure out how to use the
+ * react-markdown package to render `props.recipe` as React
+ * elements rather than the plain markdown text.
+ * 
+ * Use the link https://www.npmjs.com/package/react-markdown/v/8.0.6#use directly to the
+ * package's instructions on how to use it.
+ * 
+ * Use the `suggested-recipe-container` class on the <section>
+ * below to get some free styling 🙂
+ */
+
+export default function ClaudeRecipe(props) {
+    return (
+        <section className="suggested-recipe-container" aria-live="polite">
+            <h2>Chef Claude Recommends:</h2>
+            <ReactMarkdown>{props.recipe}</ReactMarkdown>
+        </section>
+    )
+}
