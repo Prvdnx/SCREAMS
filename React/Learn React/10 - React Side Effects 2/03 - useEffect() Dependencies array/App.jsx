0934f281c63c129544cf36c16007a66554277c6b
prvdnx <129741476+Prvdnx@users.noreply.github.com>
2025-10-14 00:29:25 +0700

    useEffect() Dependencies array

---
+++ React/Learn React/10 - React Side Effects 2/03 - useEffect() Dependencies array/App.jsx
@@ -0,0 +1,32 @@
+import React from "react"
+
+export default function App() {
+    const [starWarsData, setStarWarsData] = React.useState({})
+    const [count, setCount] = React.useState(0)
+    
+    console.log("Rendered!")
+    
+    // React.useEffect(function() {
+    //     console.log("Effect ran")
+    //     fetch("https://swapi.dev/api/people/1")
+    //         .then(res => res.json())
+    //         .then(data => setStarWarsData(data))
+    // }, [count])
+    
+    /**
+     * Challenge: re-write the useEffect
+     * It should run any time `count` changes
+     * For now, just console.log("Effect function ran")
+     */
+    React.useEffect(() => {
+        console.log("Effect function ran")
+    }, [count])
+    
+    return (
+        <div>
+            <h2>The count is {count}</h2>
+            <button onClick={() => setCount(prevCount => prevCount + 1)}>Add</button>
+            <pre>{JSON.stringify(starWarsData, null, 2)}</pre>
+        </div>
+    )
+}
