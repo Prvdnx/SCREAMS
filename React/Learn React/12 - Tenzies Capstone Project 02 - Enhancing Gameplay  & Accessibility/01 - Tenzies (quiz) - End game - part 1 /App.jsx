f1632f543e4db5fe9a92534df0f8d4a54e94026c
prvdnx <129741476+Prvdnx@users.noreply.github.com>
2025-10-14 17:58:39 +0700

    Tenzies (quiz) - End game - part 1

---
+++ React/Learn React/12 - Tenzies Capstone Project 02 - Enhancing Gameplay  & Accessibility/01 - Tenzies (quiz) - End game - part 1 /App.jsx
@@ -0,0 +1,80 @@
+import { useState } from "react"
+import Die from "./Die"
+import { nanoid } from "nanoid"
+
+export default function App() {
+    const [dice, setDice] = useState(generateAllNewDice())
+    
+    // check if the game is won
+    
+    /**
+     * Critical thinking time!
+     * 
+     * We want to indicate to the user that the game is over
+     * if (1) all the dice are held, and (2) all the dice have
+     * the same value.
+     * 
+     * How might we do this? Some questions to consider:
+     * 
+     * 1. Do we need to save a `gameWon` value in state? If so, why?
+     *    If not, why not?
+     * No.
+     * 
+     * 
+     * 2. Do we need to create a side effect to synchronize the `gameWon`
+     *    value (whether it's in state or not) with the current state of 
+     *    the dice?
+     * No.
+     * 
+     * 
+     * Conclusion:
+     * We can derive the gameWon status based on the condition(s) of the current
+     * dice state on every render.
+     */
+    
+    function generateAllNewDice() {
+        return new Array(10)
+            .fill(0)
+            .map(() => ({
+                value: Math.ceil(Math.random() * 6),
+                isHeld: false,
+                id: nanoid()
+            }))
+    }
+
+    function rollDice() {
+        setDice(oldDice => oldDice.map(die => 
+            die.isHeld ?
+                die :
+                { ...die, value: Math.ceil(Math.random() * 6) }
+        ))
+    }
+
+    function hold(id) {
+        setDice(oldDice => oldDice.map(die =>
+            die.id === id ?
+                { ...die, isHeld: !die.isHeld } :
+                die
+        ))
+    }
+
+    const diceElements = dice.map(dieObj => (
+        <Die
+            key={dieObj.id}
+            value={dieObj.value}
+            isHeld={dieObj.isHeld}
+            hold={() => hold(dieObj.id)}
+        />
+    ))
+
+    return (
+        <main>
+            <h1 className="title">Tenzies</h1>
+            <p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
+            <div className="dice-container">
+                {diceElements}
+            </div>
+            <button className="roll-dice" onClick={rollDice}>Roll</button>
+        </main>
+    )
+}
