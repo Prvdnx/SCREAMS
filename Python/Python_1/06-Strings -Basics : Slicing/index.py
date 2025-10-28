7d7dd0a16b6fc006f9dcebfbaef4c275191d6da5
prvdnx <129741476+Prvdnx@users.noreply.github.com>
2025-10-29 04:27:25 +0700

    more

---
+++ Python/Python_1/06-Strings -Basics : Slicing/index.py
@@ -0,0 +1,22 @@
+
+msg='welcome to it\'s Python 101: Strings'
+    #012345678
+
+print(msg)
+print(msg+msg)
+print(msg*2)
+print(msg,msg)
+print(msg.upper())
+print(msg.lower())
+print(msg.capitalize())
+print(msg.title())
+
+print(len(msg))
+print(msg.count('Python'))
+print(msg.count('o'))
+
+#slicing
+print(msg[2])
+print(msg[-1])
+print(msg[2:7])
+print(msg[:7])
