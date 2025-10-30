27cc7e62fd58d8d96e0bcaa0e9bd211b1d1a2e48
prvdnx <129741476+Prvdnx@users.noreply.github.com>
2025-10-31 01:49:18 +0700

    For Loops and nesting

---
+++ Python/Python_2/11-For Loops and nesting/index.py
@@ -0,0 +1,46 @@
+# for letter in 'Norwegian blue':
+#     print(letter)
+
+# for furgle in 'Norwegian blue':
+#     print(furgle)
+
+# for furgle in range(8):
+#     print(furgle)
+
+# for furgle in range(2,8):
+#     print(furgle)
+
+# for furgle in range(1,15,3): # do it in steps of 3
+#     print(furgle)
+
+# for name in ['John','Terry','Eric','Michael','George']:
+#     print(name)
+
+# friends = ['John','Terry','Eric','Michael','George']
+# for friend in friends:
+#     print(friend)
+
+# friends = ['John','Terry','Eric','Michael','George']
+# for index in range(len(friends)):
+#    print(friends[index])
+
+# friends = ['John','Terry','Eric','Michael','George']
+# for friend in friends:
+#     if friend == 'Eric':
+#         print('Found ' + friend + '!')
+#         # break
+#         continue
+#     print(friend)
+
+# friends = ['John','Terry','Eric','Michael','George']
+# for friend in friends:
+#     if friend == 'Eric':
+#         print('Found ' + friend + '!')
+#     print(friend)
+
+friends = ['John','Terry','Eric']
+for friend in friends:
+    for number in [1,2,3]:
+        print(friend, number)
+
+print("For Loop done!")
