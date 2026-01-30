dab923914756038edd421e8936ac1d23a12b6a9a
prvdnx <129741476+Prvdnx@users.noreply.github.com>
2026-01-30 18:08:40 +0700

    Use Toggle with Menu component

---
+++ React/Advanced React/1. REUSABILITY/32. Use Toggle with Menu component/index.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Toggle from "./components/Toggle/index"
+import Menu from "./components/Menu/index"
+import { BsStar, BsStarFill } from "react-icons/bs"
+
+function App() {
+
+  const sports = ["Tennis", "Pickleball", "Racquetball", "Squash"]
+  
+  return (
+    <>
+      <Toggle>
+        <Toggle.Button>
+          <Toggle.On>
+            <BsStarFill className="star filled" />
+          </Toggle.On>
+          <Toggle.Off>
+            <BsStar className="star" />
+          </Toggle.Off>
+        </Toggle.Button>
+      </Toggle>
+      
+      <br/>
+      
+      <Toggle>
+        <Menu>
+          <Toggle.Button>
+            <Menu.Button>Select</Menu.Button>
+          </Toggle.Button>
+          <Toggle.On>
+            <Menu.Dropdown>
+              {sports.map(sport => (
+                <Menu.Item key={sport}>{sport}</Menu.Item>
+              ))}
+            </Menu.Dropdown>
+          </Toggle.On>
+        </Menu>
+      </Toggle>      
+    </>
+  )
+}
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);
