6510d11630dfc239261de3d5186641042149af1b
prvdnx <129741476+Prvdnx@users.noreply.github.com>
2026-01-31 19:57:19 +0700

    Custom Hooks - useToggle part 7  -  Passed a parameter called `initialValue` to useToggle custom hook, initailValue default to `false` in case that parameter isn't provided when useToggle() is called. Initialized state with the `initialValue` parameter

---
+++ React/Advanced React/1. REUSABILITY/53. Custom Hooks - useToggle part 7/hooks/useToggle.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+export default function useToggle(initialValue = false) {
+    const [on, setOn] = React.useState(initialValue)
+
+    function toggle() {
+        setOn(prevOn => !prevOn)
+    }
+    
+    return [on, toggle]
+}
