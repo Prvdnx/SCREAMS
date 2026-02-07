30bef03d4d4cf0111a4eabece3b10579b8a91ba6
prvdnx <129741476+Prvdnx@users.noreply.github.com>
2026-02-07 21:02:41 +0700

    Create getVan function

---
+++ React/Advanced React/2. ROUTING/76. Create getVan function/api.js
@@ -0,0 +1,72 @@
+import { initializeApp } from "firebase/app"
+import {
+    getFirestore,
+    collection,
+    doc,
+    getDocs,
+    getDoc
+} from "firebase/firestore/lite"
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBcCGFxY0d5cKAzdyOk9NEtBnAu9QWP4Z4",
+  authDomain: "vanlife-22d0d.firebaseapp.com",
+  projectId: "vanlife-22d0d",
+  storageBucket: "vanlife-22d0d.firebasestorage.app",
+  messagingSenderId: "594156305447",
+  appId: "1:594156305447:web:04d4a50d20df04953ba202"
+}
+
+const app = initializeApp(firebaseConfig)
+const db = getFirestore(app)
+
+// Refactoring the fetching functions below
+const vansCollectionRef = collection(db, "vans")
+
+export async function getVans() {
+    const snapshot = await getDocs(vansCollectionRef)
+    const vans = snapshot.docs.map(doc => ({
+        ...doc.data(),
+        id: doc.id
+    }))
+    return vans
+}
+
+export async function getVan(id) {
+    const docRef = doc(db, "vans", id)
+    const snapshot = await getDoc(docRef)
+    return {
+        ...snapshot.data(),
+        id: snapshot.id
+    }
+}
+
+export async function getHostVans(id) {
+    const url = id ? `/api/host/vans/${id}` : "/api/host/vans"
+    const res = await fetch(url)
+    if (!res.ok) {
+        throw {
+            message: "Failed to fetch vans",
+            statusText: res.statusText,
+            status: res.status
+        }
+    }
+    const data = await res.json()
+    return data.vans
+}
+
+export async function loginUser(creds) {
+    const res = await fetch("/api/login",
+        { method: "post", body: JSON.stringify(creds) }
+    )
+    const data = await res.json()
+
+    if (!res.ok) {
+        throw {
+            message: data.message,
+            statusText: res.statusText,
+            status: res.status
+        }
+    }
+
+    return data
+}
