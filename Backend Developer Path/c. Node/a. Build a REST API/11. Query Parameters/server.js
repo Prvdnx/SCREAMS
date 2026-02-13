42840d5fc8a33c49b1b699fcc0a466c5bcb3551c
prvdnx <129741476+Prvdnx@users.noreply.github.com>
2026-02-13 19:18:12 +0700

    Query Parameters

---
+++ Backend Developer Path/c. Node/a. Build a REST API/11. Query Parameters/server.js
@@ -0,0 +1,14 @@
+import http from 'node:http'
+
+const server = http.createServer((req, res) => {
+
+    const urlObj = new URL(req.url, `http://${req.headers.host}`)
+    
+    // uncomment the line below to see the URL object
+    // const queryObj = Object.fromEntries(urlObj.searchParams)
+
+    console.log(queryObj)
+
+})
+
+server.listen(8000, () => console.log('Server listening on port 8000'))
