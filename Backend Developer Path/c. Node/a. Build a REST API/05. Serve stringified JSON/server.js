8acd86c39f05c7054adf0c1e18ac0b13cc573f0b
prvdnx <129741476+Prvdnx@users.noreply.github.com>
2026-02-13 18:14:42 +0700

    Serve stringified JSON

---
+++ Backend Developer Path/c. Node/a. Build a REST API/05. Serve stringified JSON/server.js
@@ -0,0 +1,14 @@
+import http from 'node:http'
+import { getDataFromDB } from './database/db.js'
+ 
+const PORT = 8003
+
+const server = http.createServer(async (req, res) => {
+  const destinations = await getDataFromDB()
+
+  if (req.url === '/api' && req.method === 'GET') {
+    res.end(JSON.stringify(destinations))
+  }
+})
+
+server.listen(PORT, () => console.log(`Connected on port: ${PORT}`))
