732a632d7594c9282ea9496b768470b89990911a
prvdnx <129741476+Prvdnx@users.noreply.github.com>
2026-02-13 17:48:15 +0700

    feat: test server to stream response data using various patterns.

--- Backend Developer Path/c. Node/a. Build a REST API/03. The HTTP module/server.js
+++ Backend Developer Path/c. Node/a. Build a REST API/03. The HTTP module/server.js
@@ -2,8 +2,19 @@ import http from 'node:http'
 
 const PORT = 8000
 
-const server = http.createServer((req, res) => {
-  res.end('Hello from the server!')
+// const server = http.createServer((req, res) => {
+//   res.end('Hello from the server!')
+// })
+
+// const server = http.createServer( (req, res)=> {
+//   res.end('This is from the server', 'utf8', ()=> console.log('response end'))
+// })
+
+const server = http.createServer( (req, res)=> {
+  res.write('This is some data \n')
+  res.write('This is some more data \n')
+  // res.end('This is from the server', 'utf8', ()=> console.log('response end'))
+  res.end()
 })
 
 server.listen(PORT, () => console.log(`server running on port: ${PORT}`))
