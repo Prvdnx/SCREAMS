40c6bb2029647bb88d797a297604ac67517f13b0
prvdnx <129741476+Prvdnx@users.noreply.github.com>
2026-02-13 19:25:40 +0700

    Query Parameters

--- Backend Developer Path/c. Node/a. Build a REST API/11. Query Parameters/server.js
+++ Backend Developer Path/c. Node/a. Build a REST API/11. Query Parameters/server.js
@@ -3,12 +3,15 @@ import http from 'node:http'
 const server = http.createServer((req, res) => {
 
     const urlObj = new URL(req.url, `http://${req.headers.host}`)
+    console.log(urlObj)
     
-    // uncomment the line below to see the URL object
-    // const queryObj = Object.fromEntries(urlObj.searchParams)
+    // http://localhost:8000/api?country=india&continent=asia
 
-    console.log(queryObj)
+    // comment the console.log(urlObj) above and uncomment the lines below to see the URL object
+    // const queryObj = Object.fromEntries(urlObj.searchParams)
+    // console.log(queryObj)
 
 })
 
-server.listen(8000, () => console.log('Server listening on port 8000'))
+server.listen(8000, () => console.log('Server listening on port 8006'))
+
